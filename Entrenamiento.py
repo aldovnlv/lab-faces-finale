53c08d358dece50a6164800bf0afd62d21d328d9
Aldo Ivan <aldovnlv@gmail.com>
2025-02-05 20:52:13 -0600

    resize imges

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -27,9 +27,25 @@ import pathlib
 # dataset = "https://drive.usercontent.google.com/download?id=17kdDJE4GnYSuNDW_7i1r31n9ReWTnIhd&export=download&authuser=0&confirm=t&uuid=b2c1cebf-4c91-4c42-b6ab-0ba704158cec&at=AIrpjvP4J4FJzIIKkGZLlSB-Qqre%3A1738345923439"
 dataset = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 directory = tf.keras.utils.get_file('flower_photos', origin=dataset, untar=True, cache_dir='.')
-os.remove('/__w/lab-faces-finale/lab-faces-finale/datasets/flower_photos/LICENSE.txt')
 data = pathlib.Path(directory)
 listaPersonas = os.listdir(data)
+# os.remove('/__w/lab-faces-finale/lab-faces-finale/datasets/flower_photos/LICENSE.txt')
+
+size = 150,150
+print('listaPersonas')
+listaPersonas.remove("LICENSE.txt")
+print(listaPersonas)
+
+for folder in listaPersonas:
+    for file in os.listdir(os.path.join(data,folder)):
+        if file.endswith("jpg"):
+            image_names.append(os.path.join(data,folder,file))
+            train_labels.append(folder)
+            img = cv2.imread(os.path.join(data,folder,file))
+            im = cv2.resize(img,size)
+            train_images.append(im)
+        else:
+            continue
 
 print("********  folder inicial")
 print(os.getcwd())
